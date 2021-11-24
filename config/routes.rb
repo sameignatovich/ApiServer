b3a265cff4fe6e624a70ef46e7daad08ef26e380
Nikita Ignatovich <nikita@ignatovich.me>
2021-11-24 12:53:40 +0300

    set default answer format to JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    resources :posts
+    resources :users
+  end
 end
