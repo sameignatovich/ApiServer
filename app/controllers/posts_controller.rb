8e63c7482db1ea66fd097219e1f749c4f4bbbd27
Nikita Ignatovich <nikita@ignatovich.me>
2021-11-24 20:32:19 +0300

    Added pagination to Posts index and items count to api output

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.page(params[:page]).per(params[:perPage])
+    @posts_count = Post.count
   end
 
   # GET /posts/1
--- app/views/posts/index.json.jbuilder
+++ app/views/posts/index.json.jbuilder
@@ -1,1 +1,4 @@
-json.array! @posts, partial: "posts/post", as: :post
+json.totalPostsCount @posts_count
+json.posts do
+  json.array! @posts, partial: "posts/post", as: :post
+end
