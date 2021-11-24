adcc479e0fca321de2f0442a3a3ae24c4c29ff61
Nikita Ignatovich <nikita@ignatovich.me>
2021-11-24 11:51:23 +0300

    Added seeds for application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 100 users'
+100.times do
+	User.create(
+		username: Faker::Internet.unique.username,
+		email: Faker::Internet.unique.email,
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		address: Faker::Address.full_address,
+		password: Faker::Internet.password,
+	)
+end
+
+puts 'Creating 1000 posts'
+1000.times do
+	Post.create(
+		title: Faker::Lorem.sentence,
+		text: Faker::Lorem.paragraph,
+		user_id: rand(1..100),
+	)
+end
