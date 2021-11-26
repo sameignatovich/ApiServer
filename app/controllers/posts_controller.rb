ba9258e848868622bf99183e9b19d6de7ea668f2
Nikita Ignatovich <nikita@ignatovich.me>
2021-11-26 12:37:28 +0300

    Posts json now give authors names

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.page(params[:page]).per(params[:perPage])
+    @posts = Post.includes(:user).page(params[:page]).per(params[:perPage])
     @posts_count = Post.count
   end
 
--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,6 @@
-json.extract! post, :id, :title, :text, :user_id, :created_at, :updated_at
+json.extract! post, :id, :title, :text, :created_at, :updated_at
+json.author do 
+  json.user_id post.user.id
+  json.username post.user.username
+end
 json.url post_url(post, format: :json)
