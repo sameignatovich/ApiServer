26084accc5a94bf8c27fa93061b5ff6b9981dd1d
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-09 18:36:41 +0300

    Fix config

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,7 +1,7 @@
 Rails.application.config.action_mailer.smtp_settings = {
   address: 'email-smtp.us-east-2.amazonaws.com',
   port: 465,
-  tls: true
+  tls: true,
   user_name: Rails.application.credentials.dig(:smtp, :username),
   password: Rails.application.credentials.dig(:smtp, :password)
 }
