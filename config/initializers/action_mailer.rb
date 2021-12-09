cea30379c0948ddaae8ed2769fbc7668ba82bc4a
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-09 18:35:15 +0300

    Added amazon SES configs

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,13 @@
+Rails.application.config.action_mailer.smtp_settings = {
+  address: 'email-smtp.us-east-2.amazonaws.com',
+  port: 465,
+  tls: true
+  user_name: Rails.application.credentials.dig(:smtp, :username),
+  password: Rails.application.credentials.dig(:smtp, :password)
+}
+
+Rails.application.config.action_mailer.delivery_method = :smtp
+
+Rails.application.config.action_mailer.default_options = {
+  from: 'info@soligorsk.place'
+}
