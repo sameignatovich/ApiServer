5e821432700ea2d05fa623e2f6335fe0852c2d7d
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-15 13:52:10 +0300

    Added localhost to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'big.wtf', 'dashboard.big.wtf', '127.0.0.1:8080'
+    origins 'big.wtf', 'dashboard.big.wtf', '127.0.0.1:8080', 'localhost:8080'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
