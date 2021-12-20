ab59fdeeb0c33c0ae00823286a6b38d4db439765
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-21 01:42:24 +0300

    Changed models, removed validations, using rich_text

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :posts, :through => :taggings
-
-  validates :name, presence: true, length: { in: 1..16 }, uniqueness: true
 end
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,4 +1,3 @@
 class Token < ApplicationRecord
-  enum status: [ :inactive, :active ]
   belongs_to :user, counter_cache: true
 end
