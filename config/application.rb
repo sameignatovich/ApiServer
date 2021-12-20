0edf4cf3ae76b651dc47869a4550ef866ed162f5
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-21 01:37:53 +0300

    New configs

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,10 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,7 +21,7 @@ Bundler.require(*Rails.groups)
 module ApiServer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -36,6 +35,8 @@ module ApiServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.generators.options[:active_record][:primary_key_type] = :uuid
 
     config.autoload_paths << Rails.root.join('lib')
   end
