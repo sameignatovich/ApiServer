e71c9eb90df7a2356bc3b10f1fcd2d652d5238ea
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-25 06:32:45 +0300

    Using predefined variants of avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   enum role: [ :blocked, :regular, :moderator, :admin ]
-  has_one_attached :avatar
   has_secure_password
   has_many :tokens, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :medium, resize_to_limit: [500, 500]
+  end
 
   default_scope { order(created_at: :desc) }
 
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
 json.extract! user, :id, :username, :email, :role, :full_name, :posts_count, :comments_count, :created_at, :updated_at
-json.avatar user.avatar.attached? ? polymorphic_url(user.avatar.variant(resize_to_limit: [500, 500])) : nil
+json.avatar user.avatar.attached? ? polymorphic_url(user.avatar.variant(:medium)) : nil
