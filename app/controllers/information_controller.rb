378169399d81c7f7654b8a2b6d0320a21fefcb6d
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-25 06:32:09 +0300

    Fixed last object extraction (we using default scoped with order)

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,9 +1,9 @@
 class InformationController < ApplicationController
   def summary
     @users_count = User.count
-    @last_user = User.last
+    @last_user = User.first
     @posts_count = Post.count
-    @last_post = Post.includes(:user).last
+    @last_post = Post.first
     @comments_count = Comment.count
     @last_comment = Comment.includes(:user).includes(:post).last
   end
